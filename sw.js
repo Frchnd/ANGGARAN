0f63cba2cb63f39faca4356949ddfd08d17aa64e
Fernando Chandra <fernandochandra2001@gmail.com>
2026-09-23 14:14:27 +0700

    chore: bump PWA cache for v0.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'anggaran-shell-v6';
+const CACHE = 'anggaran-shell-v7';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './db.js', './calc.js',
   './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'
