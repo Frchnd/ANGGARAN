55b4b51a7c85e5a7ae4344d642b29e5e97524b19
Fernando Chandra <fernandochandra2001@gmail.com>
2026-09-25 19:56:03 +0700

    chore: bump PWA cache after final responsive cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'anggaran-shell-v13';
+const CACHE = 'anggaran-shell-v14';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './db.js', './calc.js',
   './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'
